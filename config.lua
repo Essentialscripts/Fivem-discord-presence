04a9348ce67959c0ed2efff7dfb3023284e2e548
Essentialscripts <essentialscript@gmail.com>
2024-07-31 21:26:02 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,13 +2,13 @@
 Config = {}
 
 -- Your Discord Application ID
-Config.DiscordAppId = '1130231916633800818'
+Config.DiscordAppId = '#'
 
 -- Details for Rich Presence
-Config.Details = "Essential RP"
-Config.State = "In-Game"
-Config.LargeImage = "36a85bd7abf5b8bb0058f99bac4c13d0_1_"
-Config.LargeImageText = "Large Image Text"
-Config.SmallImage = "small_image"
-Config.SmallImageText = "Small Image Text"
+Config.Details = "#"
+Config.State = "#"
+Config.LargeImage = "#"
+Config.LargeImageText = "#"
+Config.SmallImage = "#"
+Config.SmallImageText = "#"
 Config.MaxPlayers = 48
